e1307588de2dc4baac6bd1aa5aac402f79d9e081
Niels Pedersen <=>
2018-05-26 10:53:38 +0200

    Add new endpoint

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, world!"
+    return 'Hello, world!'
 
-if __name__ == "__main__":
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello, ' + name
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
 
